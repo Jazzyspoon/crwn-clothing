f9fba58475a51339083b5744331b7e4a6df6b2af
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-11-08 16:23:54 -0700

    updating our app with a shop page

---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,283 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: "Hats",
+    routeName: "hats",
+    items: [
+      {
+        id: 1,
+        name: "Brown Brim",
+        imageUrl:
+          "https://cdn.shopify.com/s/files/1/0431/5161/products/dahlia-boater-brown-felt-hats-6_2000x.jpg?v=1623280328",
+        price: 25,
+      },
+      {
+        id: 2,
+        name: "Blue Beanie",
+        imageUrl:
+          "https://cdna.lystit.com/photos/5c55-2014/11/03/adidas-blue-logo-beanie-hat-product-1-25159273-0-444283899-normal.jpeg",
+        price: 18,
+      },
+      {
+        id: 3,
+        name: "Brown Cowboy",
+        imageUrl:
+          "https://i5.walmartimages.com/asr/5e716375-d39e-4533-bf10-4d1fcc2d4a9f_1.11d6c7f9be19ef0402d317d8f7a7c9a2.jpeg",
+        price: 35,
+      },
+      {
+        id: 4,
+        name: "Grey Brim",
+        imageUrl:
+          "https://cdn.shopify.com/s/files/1/0431/5161/products/scottie-wide-brim-fedora-grey-felt-hats-3_2000x.jpg?v=1629907449",
+        price: 25,
+      },
+      {
+        id: 5,
+        name: "Green Beanie",
+        imageUrl:
+          "https://i.etsystatic.com/8615558/r/il/ed7619/1555622505/il_570xN.1555622505_ndj2.jpg",
+        price: 18,
+      },
+      {
+        id: 6,
+        name: "Palm Tree Cap",
+        imageUrl:
+          "https://images.stockx.com/images/Palm-Angels-Palm-Tree-Cap-Military-Light-Blue.jpg?fit=fill&bg=FFFFFF&w=480&h=320&auto=compress&q=90&dpr=2&trim=color&updated_at=1623439975&pad=0&fm=webp",
+        price: 14,
+      },
+      {
+        id: 7,
+        name: "Red Beanie",
+        imageUrl:
+          "https://cdn.supadupa.me/shop/41379/images/2938762/Cross_Rod_Ace_www.CrossRodAce.com_men_red_slouch_Woolly_Beanie_Hat_1_massive.jpg",
+        price: 18,
+      },
+      {
+        id: 8,
+        name: "Wolf Cap",
+        imageUrl:
+          "http://cdn.shopify.com/s/files/1/2065/1359/products/ACEZ0096-1_grande.jpg?v=1499205632",
+        price: 14,
+      },
+      {
+        id: 9,
+        name: "Blue Snapback",
+        imageUrl:
+          "https://cdn.shopify.com/s/files/1/0705/2311/products/hard-luck-blue-snapback-hat-in-god-we-must-816020_800x.jpg?v=1625237508",
+        price: 16,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Sneakers",
+    routeName: "sneakers",
+    items: [
+      {
+        id: 10,
+        name: "Adidas NMD",
+        imageUrl:
+          "https://5.imimg.com/data5/UF/CR/MY-42622857/adidas-nmd-shoes-500x500.png",
+        price: 220,
+      },
+      {
+        id: 11,
+        name: "Adidas Yeezy",
+        imageUrl:
+          "https://static.highsnobiety.com/thumbor/64AZOHl0192QMPxNnCjOonCVja0=/1600x1067/static.highsnobiety.com/wp-content/uploads/2021/05/27212315/adidas-yeezy-restock-2021-00.jpg",
+        price: 280,
+      },
+      {
+        id: 12,
+        name: "Black Converse",
+        imageUrl: "https://i.ebayimg.com/images/g/mNYAAOSwQV1fWmba/s-l1600.jpg",
+        price: 110,
+      },
+      {
+        id: 13,
+        name: "Nike White AirForce",
+        imageUrl:
+          "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/2ef0932c-9be5-4937-b68e-b8d6e5679fad/air-force-1-crater-flyknit-mens-shoes-k3Jwmd.png",
+        price: 160,
+      },
+      {
+        id: 14,
+        name: "Nike Red High Tops",
+        imageUrl:
+          "https://medias.spotern.com/spots/w640/152/152524-1532336916.jpg",
+        price: 160,
+      },
+      {
+        id: 15,
+        name: "Nike Brown High Tops",
+        imageUrl:
+          "https://i.pinimg.com/736x/c6/87/42/c68742dd41296c107a1fc4d8714bcd07.jpg",
+        price: 160,
+      },
+      {
+        id: 16,
+        name: "Air Jordan Limited",
+        imageUrl:
+          "http://jordansdaily.com/wp-content/uploads/2017/02/air-jordan-31-metallic-gold-6.png",
+        price: 190,
+      },
+      {
+        id: 17,
+        name: "Timberlands",
+        imageUrl:
+          "https://images.dsw.com/is/image/DSWShoes/431048_231_ss_08?impolicy=qlt-medium-high&imwidth=640&imdensity=1",
+        price: 200,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Jackets",
+    routeName: "jackets",
+    items: [
+      {
+        id: 18,
+        name: "Black Jean Shearling",
+        imageUrl:
+          "https://m.media-amazon.com/images/I/71z8v+IL1vL._AC_UL1001_.jpg",
+        price: 125,
+      },
+      {
+        id: 19,
+        name: "Blue Jean Jacket",
+        imageUrl:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/jean-jackets-index-1613501150.jpg",
+        price: 90,
+      },
+      {
+        id: 20,
+        name: "Grey Jean Jacket",
+        imageUrl:
+          "https://images.asos-media.com/products/asos-dark-future-skinny-denim-jacket-with-embroidery-in-gray/24034926-1-grey?$n_640w$&wid=513&fit=constrain",
+        price: 90,
+      },
+      {
+        id: 21,
+        name: "Brown Shearling",
+        imageUrl:
+          "https://cdn.shopify.com/s/files/1/0017/3382/0480/products/Image_7fa6e737-dbcc-4b74-aaec-ce5019dc0d47_large.jpg?v=1610048818",
+        price: 165,
+      },
+      {
+        id: 22,
+        name: "Tan Trench",
+        imageUrl:
+          "https://cdn.shopify.com/s/files/1/0108/9998/0346/products/honey_trench_coat_casual_outfits.jpg?v=1632843187",
+        price: 185,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Womens",
+    routeName: "womens",
+    items: [
+      {
+        id: 23,
+        name: "Blue Tanktop",
+        imageUrl:
+          "https://cdna.lystit.com/photos/e1b6-2015/09/18/nike-game-royalreflective-silver-dri-fittm-contour-tank-top-silver-product-2-456119875-normal.jpeg",
+        price: 25,
+      },
+      {
+        id: 24,
+        name: "Floral Blouse",
+        imageUrl:
+          "https://pimg.bucklecontent.com/images/products/81196GJ3175WYB/BEI/f/f4849b6b86e4dc31f82ab635b1b8c3bdv3?cropH=1880&cropW=1603&quality=0.8&width=845",
+        price: 20,
+      },
+      {
+        id: 25,
+        name: "Floral Dress",
+        imageUrl:
+          "https://media1.popsugar-assets.com/files/thumbor/ZorjRsYVLIZgEg-gJj4ToMBVNiQ/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2020/02/13/947/n/1922564/978fbd171b8ba5f6_netimg1chfjv/i/Drop-Blush-Pink-Floral-V-Neck-Button-Down-Tiered-Maxi-Dress-by-charlottebridgeman.jpg",
+        price: 80,
+      },
+      {
+        id: 26,
+        name: "Red Dots Dress",
+        imageUrl:
+          "https://ae01.alicdn.com/kf/HTB1T9xjRXzqK1RjSZFoq6zfcXXaK/Red-White-Polka-Dot-Dress-2019-Summer-Sleeveless-Tank-Tunic-Big-Swing-Pinup-Dress-Vintage-Party.jpg_Q90.jpg_.webp",
+        price: 80,
+      },
+      {
+        id: 27,
+        name: "Striped Sweater",
+        imageUrl:
+          "https://i5.walmartimages.com/asr/12fb5172-1a3f-45b2-a5ae-5c1fffa3f802_1.8b9260a700dbd5096f9382ecafc202d5.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+        price: 45,
+      },
+      {
+        id: 28,
+        name: "Yellow Track Suit",
+        imageUrl:
+          "http://cdn.shopify.com/s/files/1/0065/2650/0934/products/stripped-womens-2-piece-tracksuit-set-yellow-pant-979.jpg?v=1623603283",
+        price: 135,
+      },
+      {
+        id: 29,
+        name: "White Blouse",
+        imageUrl:
+          "https://sc04.alicdn.com/kf/H829d7f6e260a444ca9ef0836a2832c857.jpg",
+        price: 20,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Mens",
+    routeName: "mens",
+    items: [
+      {
+        id: 30,
+        name: "Camo Down Vest",
+        imageUrl:
+          "https://slimages.macysassets.com/is/image/MCY/products/6/optimized/16026976_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1",
+        price: 325,
+      },
+      {
+        id: 31,
+        name: "Floral T-shirt",
+        imageUrl:
+          "https://i.pinimg.com/originals/06/be/5e/06be5e1c69f081f901e751c621a4cfb7.png",
+        price: 20,
+      },
+      {
+        id: 32,
+        name: "Black & White Longsleeve",
+        imageUrl:
+          "https://ae01.alicdn.com/kf/HTB1TAvlSpXXXXXmXpXXq6xXFXXXN/Classic-Striped-Men-s-T-Shirts-Long-Sleeve-Black-White-Thick-Casual-T-Shirt-Men-Autumn.jpg",
+        price: 25,
+      },
+      {
+        id: 33,
+        name: "Pink T-shirt",
+        imageUrl:
+          "https://i.pinimg.com/736x/3a/8c/65/3a8c65f7a894751b7570002ca578d5ab.jpg",
+        price: 25,
+      },
+      {
+        id: 34,
+        name: "Jean Long Sleeve",
+        imageUrl:
+          "https://target.scene7.com/is/image/Target/GUEST_3f428ead-8421-4614-8d47-f4b7ff20ec24?wid=488&hei=488&fmt=pjpeg",
+        price: 40,
+      },
+      {
+        id: 35,
+        name: "Burgundy T-shirt",
+        imageUrl:
+          "https://cdn.lookastic.com/looks/burgundy-crew-neck-t-shirt-black-sweatpants-white-low-top-sneakers-large-71059.jpg",
+        price: 25,
+      },
+    ],
+  },
+];
+
+export default SHOP_DATA;
