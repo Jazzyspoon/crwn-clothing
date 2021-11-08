f8952d1bd5c07a2cd1c530c0b74dbfe30acc89f7
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-11-08 12:03:05 -0700

    working routes so far

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from "react";
 import HomePage from "./pages/homepage/homepage-component";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+// const JacketsPage = () => (
+//   <div>
+//     <h1>Jackets Page</h1>
+//   </div>
+// );
+// const SneakersPage = () => (
+//   <div>
+//     <h1>Sneakers Page</h1>
+//   </div>
+// );
+
+// const WomensPage = () => (
+//   <div>
+//     <h1>Womens Page</h1>
+//   </div>
+// );
+
+// const MensPage = () => (
+//   <div>
+//     <h1>Mens Page</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+        {/* <Route path="/jackets" element={<JacketsPage />} />
+        <Route path="/sneakers" element={<SneakersPage />} />
+        <Route path="/womens" element={<WomensPage />} />
+        <Route path="/mens" element={<MensPage />} /> */}
+      </Switch>
     </div>
   );
 }
