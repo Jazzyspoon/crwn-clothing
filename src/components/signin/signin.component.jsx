218e1226fc9af0cbe38b5aa797f5b287fd973d9b
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-11-09 12:01:42 -0700

    integrated firebase and sign in component

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import "./signin.styles.scss";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor(props) {
@@ -46,7 +47,13 @@ class SignIn extends React.Component {
             required
             label="Password"
           />
-          <CustomButton type="submit"> Sign In</CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign In</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {" "}
+              Sign In With Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "AIzaSyBXcaPwLAvhY7eFfSPyKnYxKRouyQkzG74",
+  authDomain: "crwn-db-a1773.firebaseapp.com",
+  projectId: "crwn-db-a1773",
+  storageBucket: "crwn-db-a1773.appspot.com",
+  messagingSenderId: "1085960834094",
+  appId: "1:1085960834094:web:7ced8ac2b5de72a13a63bf",
+  measurementId: "G-YWTWNNTXLV",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
