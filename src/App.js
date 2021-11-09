b8e6be7c58d35e2446a11211320a12df6508d99f
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-11-09 13:09:57 -0700

    updated firebase utils included storage on firebase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup-page";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import "./App.css";
 
 class App extends React.Component {
@@ -18,9 +18,20 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/signin-and-signup/signin-and-signup-page.jsx
+++ src/pages/signin-and-signup/signin-and-signup-page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SignIn from "../../components/signin/signin.component";
+import SignUp from "../../components/signup/signup.component";
 import "./signin-and-signup-page.styles.scss";
 
 const SigninAndSignupPage = () => (
   <div className="signin-and-signup-page">
-    <h1>
-      <SignIn />
-    </h1>
+    <SignIn />
+    <SignUp />
   </div>
 );
 
