fcd3c5db94eefcec321f0082c0061df2707cc591
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-11-08 17:21:48 -0700

    updated sign in form

---
+++ src/pages/signin-and-signup/signin-and-signup-page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SignIn from "../../components/signin/signin.component";
+import "./signin-and-signup-page.styles.scss";
+
+const SigninAndSignupPage = () => (
+  <div className="signin-and-signup-page">
+    <h1>
+      <SignIn />
+    </h1>
+  </div>
+);
+
+export default SigninAndSignupPage;
